f4050a98e63242da8a3333f057a2ae6579f9f39b
Ozzy Ibrahim <ozzy.ibrahim@justice.com>
2024-03-20 20:08:58 +0000

    update terraform main.tf for azure resources

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 30001;
 
 // Define the route for the app server
 app.get('/', (req, res) => {
--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,11 @@ terraform {
   }
 }
 
+# Azure Provider configuration
+provider "azurerm" {
+  features {}
+}
+
 # Docker network
 resource "docker_network" "webapp_network" {
   name   = "webapp_network"
@@ -46,6 +51,7 @@ resource "docker_service" "app" {
     }
   }
 }
+
 
 # Docker service for database
 
@@ -87,4 +93,57 @@ resource "docker_service" "web" {
 }
 
 
+##############################
+#      Azure resources       #
+##############################
 
+
+ # Create a resource group
+# resource "azurerm_resource_group" "rg" {
+#   name     = "oz-rg"
+#   location = "uksouth"
+# }
+
+# # Create an AKS instance
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                = "OZAKSCluster"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   dns_prefix          = "oz-aks"
+
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 1
+#     vm_size    = "Standard_D2_v2"
+#   }
+
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
+
+######## Azure database for MySQL ########
+
+# resource "azurerm_mysql_server" "oz-mysql" {
+#   name                = "oz-mysql"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   sku_name            = "B_Gen5_1"
+#   version             = "5.7"
+#   storage_profile {
+#     storage_mb            = 5120
+#     backup_retention_days = 7
+#     geo_redundant_backup  = "Disabled"
+#   }
+#   administrator_login          = "mysqladminun"
+#   administrator_login_password = "dbxadminpw*"
+#   ssl_enforcement_enabled      = false
+# }
+
+# resource "azurerm_mysql_database" "oz-db" {
+#   name                = "oz-database"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   server_name         = azurerm_mysql_server.oz-mysql.name
+#   charset             = "utf8"
+#   collation           = "utf8_unicode_ci"
+# }
